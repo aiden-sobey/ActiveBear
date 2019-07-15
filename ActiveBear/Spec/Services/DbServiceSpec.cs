ee82d53baf2c226c87bbada181c28d246588bec0
Aiden Sobey <sobey.aiden@gmail.com>
2019-07-16 07:59:30 +0800

    Add DbService spec.
    Spec coverage for the creating of DB instances through this service.

---
+++ ActiveBear/Spec/Services/DbServiceSpec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ActiveBear.Hubs;
+using ActiveBear.Models;
+using ActiveBear.Services;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace ActiveBear.Spec.Services
+{
+    [TestFixture]
+    public class DbServiceSpec
+    {
+        internal const string Lorem = "Lorem";
+
+        [Test]
+        public async Task NewDbContextCreatedSuccessfully()
+        {
+            var user = await UserService.CreateUser(Lorem, Lorem, Lorem);
+            await ChannelService.CreateChannel(Lorem, Lorem, user);
+
+            var context = DbService.NewDbContext();
+            Assert.IsNotEmpty(context.Users.ToList());
+            Assert.IsNotEmpty(context.Channels.ToList());
+        }
+    }
+}
