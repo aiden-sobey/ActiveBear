d5a38e4c9c3c20628ead95578e1f16ae06436b47
Aiden Sobey <sobey.aiden@gmail.com>
2019-07-16 22:05:39 +0800

    Remove encryption services.
    Removed the AES encrypt and AES decrypt methods from the encryption
    service as they arn't used and very likely won't be.

--- ActiveBear/Services/EncryptionService.cs
+++ ActiveBear/Services/EncryptionService.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Security.Cryptography;
-using System.IO;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 namespace ActiveBear.Services
-{
-    // Code is taken from https://stackoverflow.com/questions/273452/using-aes-encryption-in-c-sharp
-   
+{   
     public static class EncryptionService
     {
-        private static int _iterations = 2;
-        private static int _keySize = 256;
-
-        private static string _hash = "SHA1";
-        private static string _salt = "aselrias38490a32"; // Random
-        private static string _vector = "8947az34awl34kjq"; // Random
-
         // TODO: async?
         public static string Sha256(string rawData)
         {
@@ -32,92 +21,5 @@ namespace ActiveBear.Services
                 return builder.ToString();
             }
         }
-
-        public static string AesEncrypt(string value, string password)
-        {
-            return AesEncrypt<AesManaged>(value, password);
-        }
-
-        public static string AesEncrypt<T>(string value, string password)
-                where T : SymmetricAlgorithm, new()
-        {
-            byte[] vectorBytes = Encoding.ASCII.GetBytes(_vector);
-            byte[] saltBytes = Encoding.ASCII.GetBytes(_salt);
-            byte[] valueBytes = Encoding.UTF8.GetBytes(value);
-
-            byte[] encrypted;
-            using (T cipher = new T())
-            {
-                PasswordDeriveBytes _passwordBytes =
-                    new PasswordDeriveBytes(password, saltBytes, _hash, _iterations);
-                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
-
-                cipher.Mode = CipherMode.CBC;
-
-                using (ICryptoTransform encryptor = cipher.CreateEncryptor(keyBytes, vectorBytes))
-                {
-                    using (MemoryStream to = new MemoryStream())
-                    {
-                        using (CryptoStream writer = new CryptoStream(to, encryptor, CryptoStreamMode.Write))
-                        {
-                            writer.Write(valueBytes, 0, valueBytes.Length);
-                            writer.FlushFinalBlock();
-                            encrypted = to.ToArray();
-                        }
-                    }
-                }
-                cipher.Clear();
-            }
-            return Convert.ToBase64String(encrypted);
-        }
-
-        public static string AesDecrypt(string value, string password)
-        {
-            return AesDecrypt<AesManaged>(value, password);
-        }
-        public static string AesDecrypt<T>(string value, string password) where T : SymmetricAlgorithm, new()
-        {
-            // TODO need much better error handling here - if we use it
-            if (string.IsNullOrEmpty(value) || !value.EndsWith("="))
-                return "Invalid input- string isn't AES encrypted.";
-
-            byte[] vectorBytes = Encoding.ASCII.GetBytes(_vector);
-            byte[] saltBytes = Encoding.ASCII.GetBytes(_salt);
-            byte[] valueBytes = Convert.FromBase64String(value);
-
-            byte[] decrypted;
-            int decryptedByteCount = 0;
-
-            using (T cipher = new T())
-            {
-                PasswordDeriveBytes _passwordBytes = new PasswordDeriveBytes(password, saltBytes, _hash, _iterations);
-                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
-
-                cipher.Mode = CipherMode.CBC;
-
-                try
-                {
-                    using (ICryptoTransform decryptor = cipher.CreateDecryptor(keyBytes, vectorBytes))
-                    {
-                        using (MemoryStream from = new MemoryStream(valueBytes))
-                        {
-                            using (CryptoStream reader = new CryptoStream(from, decryptor, CryptoStreamMode.Read))
-                            {
-                                decrypted = new byte[valueBytes.Length];
-                                decryptedByteCount = reader.Read(decrypted, 0, decrypted.Length);
-                            }
-                        }
-                    }
-                }
-                catch
-                {
-                    return string.Empty;
-                }
-
-                cipher.Clear();
-            }
-            return Encoding.UTF8.GetString(decrypted, 0, decryptedByteCount);
-        }
     }
-    
 }
